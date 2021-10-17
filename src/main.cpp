a5dd7f7e06427cd020745e64ee83040884469436
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-17 21:20:03 +0100

    Made linking to lib1 work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "example_cpp_app/foo.hpp"
+#include "example_cpp_lib1/foo.hpp"
 
 int main() {
-  example_cpp_app::Foo foo{"example-cpp-app"};
+  example_cpp_lib1::Foo foo{"example-cpp-app"};
   (&foo)->printMsg();
 
   return 0;
