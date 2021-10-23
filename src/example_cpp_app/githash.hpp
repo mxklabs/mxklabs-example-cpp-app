525b0fe55d8a9e90c57fa5f928e8b37be20caedb
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-10-23 14:29:08 +0100

    Added ability to read git hash

---
+++ src/example_cpp_app/githash.hpp
@@ -0,0 +1,12 @@
+#ifndef MXKLABS_EXAMPLE_CPP_APP_GITHASH_HPP
+#define MXKLABS_EXAMPLE_CPP_APP_GITHASH_HPP
+
+#include <string>
+
+namespace example_cpp_app {
+
+extern const std::string gitHash;
+
+}
+
+#endif
